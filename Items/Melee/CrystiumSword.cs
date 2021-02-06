d081596b4abad7fe7353c8127f7750c2a619513e
Chinzilla00 <Mitchell_Pepe@yahoo.com>
2021-02-06 12:23:37 -0500

    sord of huh

--- Items/Melee/CrystiumSword.cs
+++ Items/Melee/CrystiumSword.cs
@@ -1,3 +1,6 @@
+using Annihilation.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -11,21 +14,31 @@ namespace Annihilation.Items.Melee
 		}
 		public override void SetDefaults()
         {
-			item.damage = 24;
+			item.damage = 16;
 			item.melee = true;
 			item.width = 46;
             item.height = 56;
-			item.useTime = 17;
-			item.useAnimation = 17;
+			item.useTime = 32;
+			item.useAnimation = 32;
 			item.useStyle = ItemUseStyleID.SwingThrow;
 			item.noMelee = false;
-			item.knockBack = 6;
+			item.knockBack = 6f;
 			item.value = 10000;
 			item.rare = ItemRarityID.Green;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
-			item.shoot = 0;
-            item.shootSpeed = 4f;
+            item.shoot = ModContent.ProjectileType<SwordCrystal>();
+            item.shootSpeed = 8f;
 		}
-	}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 3;
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(80));
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return false;
+        }
+    }
 }
---
+++ Projectiles/SwordCrystal.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Annihilation.Projectiles
+{
+    class SwordCrystal : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crystal Spike");
+            Main.projFrames[projectile.type] = 3;
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 24;
+            projectile.damage = 10;
+            projectile.aiStyle = -1;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.melee = true;
+            projectile.penetrate = 1;
+            projectile.ignoreWater = false;
+            projectile.tileCollide = true;
+            projectile.timeLeft = 600;
+        }
+        private int type = Main.rand.Next(3);
+        public override void AI()
+        {
+            if (projectile.ai[0] == 0)
+            {
+                if (type == 0)
+                {
+                    projectile.damage -= 3;
+                    projectile.frame += 2;
+                }
+                else if (type == 1)
+                {
+                    projectile.damage += 4;
+                    projectile.frame += 1;
+                }
+                projectile.ai[0] = 1;
+            }
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(-90f);
+        }
+    }
+}
