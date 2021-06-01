66bd60a56c591883414c360d43977943a6825e03
Delta Bec Noir <coolcraft938@gmail.com>
2021-05-31 21:08:49 -0700

    more stuff
    renamed ansigma to ansolar for now, and removed the old ansolar song, and took out the boss themes for the later bosses for now, and turned them into ogg's for better in-game looping. and added the second kulvectus projectile and the crystium rock tile and item

---
+++ Items/Materials/CrystiumRock.cs
@@ -0,0 +1,31 @@
+﻿using Annihilation.Tiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Annihilation.Items.Materials
+{
+    public class CrystiumRock : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crystium Rock");
+            Tooltip.SetDefault("A crystalline metalic rock");
+        }
+        public override void SetDefaults()
+        {
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.width = 22;
+            item.height = 18;
+            item.maxStack = 999;
+            item.value = 100;
+            item.rare = ItemRarityID.Green;
+            item.createTile = ModContent.TileType<CrystiumRockTile>();
+        }
+    }
+}
--- NPCs/Ansolar/Ansolar.cs
+++ NPCs/Ansolar/Ansolar.cs
@@ -40,7 +40,7 @@ namespace Annihilation.NPCs.Ansolar
             npc.HitSound = SoundID.NPCHit42;
             npc.DeathSound = SoundID.NPCDeath14;
             npc.buffImmune[BuffID.Confused] = true;
-            music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/Ansigma");
+            music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/Ansolar");
         }
         public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
         {
